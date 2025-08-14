c3a69aea93b46f523c54fbb5d85f3edf884bc40b
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2025-08-14 02:17:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require('http');
-const port = 3000;
+
+const port = 80;
+const host = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello from my Node.js server on DigitalOcean!\n');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
 });
